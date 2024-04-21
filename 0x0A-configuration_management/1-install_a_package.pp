62aac9f1d5048352bae7bc77efc9886668e440ae
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-04-21 16:58:27 +0100

    task 1: dependency versions need to be specified

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,26 @@
-# Install flask from pip3
+# Install flask from pip3 with specific dependancy versions
 
+# Ensure Python 3.8.10 is installed
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# And that the pip package for insatalling flask in installed
 package{
   'python3-pip':
     ensure => installed,
+}
+
+# Ensure werkzeug 2.1.1 is installed
+package{
+  'werkzeug':
+    ensure   =>'2.1.1',
+    provider => 'pip3',
 }
 
 package{
 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip']
+  require  => Package['python3-pip', 'werkzeug'];
 }
