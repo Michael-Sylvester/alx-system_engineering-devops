04be57b586a7977aaf397b9b04cbcec3211a86ce
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-04-25 01:20:54 +0100

    task 5 update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,28 @@
-# Install Nginx and configure server block
+
 package { 'nginx':
   ensure => installed,
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-    content => "
-    server {
-      listen 80;
-      listen [::]:80;
-      server_name _;
-
-      root /var/www/html;
-      index index.html index.htm;
-
-      location / {
-        try_files \$uri \$uri/ =404;
-        echo 'Hello World!';
-      }
-
-      location /redirect_me {
-        return 301 https://www.example.com;
-      }
-    }
-    ",
-}
-
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => "Hello World!",
-}
-
-# Restart Nginx service
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
 }
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+
+  location / {
+    return 200 'Hello World!';
+    add_header Content-Type text/plain;
+  }
+
+  location /redirect_me {
+    return 301 http://michaelsylvester.tech/;
+  }
+}",
+    notify  => Service['nginx'],
+  }
