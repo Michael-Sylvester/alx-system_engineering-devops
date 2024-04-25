2c331e3160e481a90e4c95024864a567d36860a8
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-04-25 01:14:38 +0100

    task 5 update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,45 +1,39 @@
-# Install Nginx, server should listen on port 80 and redirect queries to /redirect_me
-
-package{'nginx':
+# Install Nginx and configure server block
+package { 'nginx':
   ensure => installed,
 }
 
-service{'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+    content => "
+    server {
+      listen 80;
+      listen [::]:80;
+      server_name _;
+
+      root /var/www/html;
+      index index.html index.htm;
+
+      location / {
+        try_files \$uri \$uri/ =404;
+        echo 'Hello World!';
+      }
+
+      location /redirect_me {
+        return 301 https://www.example.com;
+      }
+    }
+    ",
 }
 
-file{'/etc/nginx/sites-available/default':
-  ensure   => present,
-  content  => "server {
-    listen 80;
-    listen [::]:80;
-
-    root /var/www/html;
-    index index.html index.htm;
-
-    server_name _;
-
-    location /redirect_me {
-      return 301 http://michaelsylvester.tech;
-    }
-
-    location / {
-      return 200 'Hello World!';
-      add_header Content-Type text/plain;
-    }
-
-}",
-    notify  => Exec['restart nginx'],
-}
-
-file{'/var/www/html/index.html':
+file { '/var/www/html/index.html':
   ensure  => present,
   content => "Hello World!",
 }
 
-exec{'restart nginx':
-  command     => 'service nginx reload',
-  refreshonly => true,
+# Restart Nginx service
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
