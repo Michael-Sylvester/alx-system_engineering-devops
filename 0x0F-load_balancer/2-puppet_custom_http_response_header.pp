b0010ed4fa933b45df1ff6a69645553bd955aef5
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-05-03 11:50:14 +0100

    task 3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,53 +1,24 @@
-# 2-puppet_custom_http_response_header.pp
+# configures an ubuntu server using puppet in the manner of task 0:
 
-# Install HAProxy package
-package { 'haproxy':
+exec { 'apt_update':
+  command => '/usr/bin/apt-get update',
+}
+
+-> package { 'nginx' :
   ensure => installed,
 }
 
-# Configure HAProxy
-file { '/etc/haproxy/haproxy.cfg':
-  ensure  => file,
-  content => template('haproxy/haproxy.cfg.erb'), # Use a template to generate the configuration
-  require => Package['haproxy'],
-  notify  => Service['haproxy'],
+-> file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
 }
 
-# Template for HAProxy configuration
-# haproxy/haproxy.cfg.erb
-<% @hostname = fqdn_scope() %>
-global
-log /dev/log    local0
-log /dev/log    local1 notice
-chroot /var/lib/haproxy
-stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
-stats timeout 30s
-user haproxy
-group haproxy
-daemon
+-> file_line { 'add header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
 
-defaults
-log     global
-mode    http
-option  httplog
-option  dontlognull
-timeout connect 5000
-timeout client  50000
-timeout server  50000
-
-frontend http_front
-bind *:80
-stats uri /haproxy?stats
-default_backend http_back
-
-backend http_back
-balance roundrobin
-server web1 <web1_ip>:80 check
-server web2 <web2_ip>:80 check
-
-# Restart HAProxy service
-service { 'haproxy':
-  ensure    => running,
-  enable    => true,
-  require   => File['/etc/haproxy/haproxy.cfg'],
+-> service { 'nginx':
+  ensure => running,
 }
